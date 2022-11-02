9ebecd4a500feed3842ffa11ca44175de6f10443
Dak Lod <trandacloc123@gmail.com>
2022-11-02 13:08:06 +0700

    thay doi lai cau truc folder

--- js/var.js
+++ js/var.js
@@ -1,12 +1,53 @@
-var categories  = 
-['Trang chủ', 'Nam', 'Nữ', 'Trẻ em', 'Khuyến mãi']
-var info = {
-    'phone-num' : "+84337961759",
-    'email' : 'trandacloc123@gmail.com',
-    'address' : 'Đại học Sài Gòn'
+var categories = localStorage.getItem('cate')
+categories = JSON.parse(categories)
+if (!categories){
+    categories  = 
+    ['Trang chủ', 'Nam', 'Nữ', 'Trẻ em', 'Khuyến mãi']
+    localStorage.setItem('cate', JSON.stringify(categories))
+}
+
+var info = localStorage.getItem('info')
+info = JSON.parse(info)
+
+if (!info) {
+    info = {
+        'phone-num' : "+84337961759",
+        'email' : 'trandacloc123@gmail.com',
+        'address' : 'Đại học Sài Gòn'
+    }
+    localStorage.setItem('info', JSON.stringify(info))
 }
 
 
+var account = localStorage.getItem('acc')
+account = JSON.parse(account)
+if (!account) {
+    account = [
+        {
+            username: 'admin',
+            password: 'admin'
+        }
+    ]
+    localStorage.setItem('acc', JSON.stringify(account))
+}
+
+var products = localStorage.getItem('product')
+products = JSON.stringify(products)
+if (!product) {
+    var product = function (id, name, price, decrip, img){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.decrip = decrip;
+        this.img = img
+    }
+    
+    
+    var tmp = new product(1,'Giày Thể Thao Nam Hunter Street Cream DSMH10400KEM (Kem)',781000,'Miêu tả:  ', '.img/product1.webp')
+    
+    products = []
+    products.push(tmp)
+}
 
 
 function renData(){
@@ -35,6 +76,9 @@ function renData(){
         // link.appendChild(span)
         navBar.appendChild(link)
     })
+
+    $('#body').load('admin.html') //Sửa admin.html thành tên file của mình
+
 
 }
 
